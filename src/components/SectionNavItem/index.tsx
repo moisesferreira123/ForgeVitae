65436695da83886d06d3aee2988b6a1ae9e4a288
Moisés Ferreira <moisesaoaoao@gmail.com>
2026-04-09 00:16:51 -0300

    Drag and drop concluído

--- src/components/SectionNavItem/index.tsx
+++ src/components/SectionNavItem/index.tsx
@@ -1,40 +1,81 @@
 import { GripVertical, Trash2 } from "lucide-react";
 import type { SectionItem } from "../../types/sectionItem";
 import { useSectionsStore } from "../../store/sectionsStore";
+import { Draggable } from "@hello-pangea/dnd";
 
-export default function SectionNavItem( {sectionItem, isActive, onSelectSection} : {sectionItem: SectionItem, isActive: boolean, onSelectSection: (sectionId: number) => void}) {
+interface SectionNavItemProps {
+  sectionItem: SectionItem;
+  isActive: boolean;
+  position: number;
+  onSelectSection: (sectionId: number) => void;
+}
+
+export default function SectionNavItem({sectionItem, isActive, position, onSelectSection} : SectionNavItemProps) {
   const {id, label, icon: Icon} = sectionItem;
   const sectionsStore = useSectionsStore();
 
   function removeSection(e: React.MouseEvent) {
     e.stopPropagation();
     sectionsStore.removeSection(id);
+    if(isActive) onSelectSection(sectionsStore.ids[position-1]);
   }
 
+  if(id === 0) return (
+    <div className={`w-full`}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => onSelectSection(id)}
+        className={`w-full flex justify-between items-center gap-3 p-3 rounded-xl transition-all duration-300 cursor-pointer ${isActive ? 'bg-(--primary) text-(--foreground) ' : 'text-(--muted-foreground) hover:bg-(--muted) hover:text-(--foreground)'}`}
+      >
+        <div className="flex items-center gap-3">
+          <div className={`shrink-0 w-8 h-8 rounded-full flex justify-center items-center ${isActive ? 'bg-(--foreground)/20' : 'bg-(--muted)'}`}>
+            <Icon size={16} />
+          </div>
+          <span className="font-medium text-sm text-start">{label}</span>
+        </div>
+      </div>
+    </div>
+  );
+  
+
   return (
-    <button
-      onClick={() => onSelectSection(id)}
-      className={`w-full flex justify-between items-center gap-3 p-3 rounded-xl transition-all duration-300 cursor-pointer ${isActive ? 'bg-(--primary) text-(--foreground) ' : 'text-(--muted-foreground) hover:bg-(--muted) hover:text-(--foreground)'}`}
-    >
-      <div className="flex items-center gap-3">
-        <div className={`shrink-0 w-8 h-8 rounded-full flex justify-center items-center ${isActive ? 'bg-(--foreground)/20' : 'bg-(--muted)'}`}>
-          <Icon size={16} />
-        </div>
-        <span className="font-medium text-sm text-start">{label}</span>
-      </div>
-      <div className="flex items-center gap-3 ">
-        <button 
-          onClick={removeSection}
-          className="z-10 hover:text-red-400 cursor-pointer"
+    <Draggable draggableId={`${position}`} index={position} >
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          className={`w-full`}
         >
-          <Trash2 size={16} />
-        </button>
-        <div 
-          className={`flex flex-none z-10 justify-center items-center cursor-grab rounded-md hover:text-(--foreground)/70`}
-        >
-          <GripVertical size={18} />
+          <div
+            role="button"
+            tabIndex={0}
+            onClick={() => onSelectSection(id)}
+            className={`w-full flex justify-between items-center gap-3 p-3 rounded-xl transition-all duration-300 cursor-pointer ${isActive ? 'bg-(--primary) text-(--foreground) ' : 'text-(--muted-foreground) hover:bg-(--muted) hover:text-(--foreground)'}`}
+          >
+            <div className="flex items-center gap-3">
+              <div className={`shrink-0 w-8 h-8 rounded-full flex justify-center items-center ${isActive ? 'bg-(--foreground)/20' : 'bg-(--muted)'}`}>
+                <Icon size={16} />
+              </div>
+              <span className="font-medium text-sm text-start">{label}</span>
+            </div>
+            <div className="flex items-center gap-3 ">
+              <button 
+                onClick={removeSection}
+                className="z-10 hover:text-red-400 cursor-pointer"
+              >
+                <Trash2 size={16} />
+              </button>
+              <div 
+                {...provided.dragHandleProps}
+                className={`flex flex-none z-10 justify-center items-center cursor-grab rounded-md hover:text-(--foreground)/70`}
+              >
+                <GripVertical size={18} />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </button>
+      )}
+    </Draggable>
   );
 }
--- src/components/SectionsSidebar/index.tsx
+++ src/components/SectionsSidebar/index.tsx
@@ -1,3 +1,4 @@
+import { DragDropContext, Droppable, type DropResult } from "@hello-pangea/dnd";
 import { allSections } from "../../constants/allSections";
 import { useSectionsStore } from "../../store/sectionsStore";
 import SectionNavItem from "../SectionNavItem";
@@ -5,19 +6,40 @@ import SectionNavItem from "../SectionNavItem";
 export default function SectionsSidebar({ activedSectionId, onSelectSection } : { activedSectionId: number, onSelectSection: (sectionId: number) => void}) {
   const sectionsStore = useSectionsStore();
 
+  function reorder(result: DropResult) {
+    if(!result.destination) return;
+
+    const updatedSectionsIds = [...sectionsStore.ids];
+    const [reorderedsectionsIds] = updatedSectionsIds.splice(result.source.index, 1);
+    updatedSectionsIds.splice(result.destination.index, 0, reorderedsectionsIds);
+
+    sectionsStore.updateSections(updatedSectionsIds);
+  }
+
   return (
-    <div className="space-y-2 h-[calc(100vh-222px)] overflow-y-auto no-scrollbar py-2">
-      {allSections.map((item) => {
-        if(!sectionsStore.ids.includes(item.id)) return;
-        return (
-          <SectionNavItem 
-            key={item.id} 
-            sectionItem={item}
-            isActive={item.id === activedSectionId}
-            onSelectSection={onSelectSection}
-          />
-        )
-      })}
+    <div className="h-[calc(100vh-222px)] overflow-y-auto no-scrollbar py-2">
+      <DragDropContext onDragEnd={reorder} >
+        <Droppable droppableId="sections-nav" type="list" direction="vertical" >
+          {(provided) => ( 
+            <div 
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex flex-col gap-2 w-full "
+            >
+              {sectionsStore.ids.map((id, index) => (
+                  <SectionNavItem 
+                    key={id} 
+                    sectionItem={allSections[id]}
+                    isActive={id === activedSectionId}
+                    position={index}
+                    onSelectSection={onSelectSection}
+                  />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
